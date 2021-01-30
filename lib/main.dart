86c2e0cc7f8e4cc7735f322f905f2b57b863bbe8
Gaspar Habif <gasparhabif@gmail.com>
2021-01-29 21:27:33 -0300

    Developed starting buttons for menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,29 +18,50 @@ class MyApp extends StatelessWidget {
         primaryTextTheme: Typography.whiteRedmond,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+Color toColor(hex) => Color(int.parse("0xff" + hex));
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final _menu = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _menuButton("Empezar", () {}),
+        const SizedBox(height: 20),
+        _menuButton("Continuar", () {}),
+        const SizedBox(height: 20),
+        _menuButton("Ajustes", () {}),
+      ],
+    );
+
+    return Scaffold(
+      body: Center(child: _menu),
     );
   }
 
-  Color toColor(hex) => Color(int.parse("0xff" + hex));
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contador de Bazas'),
-      ),
-      // body is the majority of the screen.
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
-    );
+  RaisedButton _menuButton(text, function) {
+    return RaisedButton(
+        onPressed: function,
+        color: toColor("1DE9B6"),
+        textColor: Colors.white,
+        child: Container(
+            width: 200,
+            padding: const EdgeInsets.all(10),
+            child: Center(
+              child: Text(text,
+                  style: GoogleFonts.poppins(
+                      fontStyle: FontStyle.normal, fontSize: 36)),
+            )));
   }
 }
